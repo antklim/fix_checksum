14d0378aff0620e2ce766a8d0487f638b4f933cb
antklim <antklim@gmail.com>
2015-10-26 23:07:12 +1100

    Checksum validation implementation

--- src/lib.rs
+++ src/lib.rs
@@ -8,7 +8,7 @@
 //! # Examples
 //!
 //! ```
-//! assert_eq!(true, fix_checksum::validate());
+//! assert_eq!(false, fix_checksum::validate(""));
 //!
 //! let outbound_message = "8=FIX.4.2|9=73|35=0|49=BRKR|56=INVMGR|34=235|52=19980604-07:58:28|112=19980604-07:58:28|";
 //! assert_eq!("196", fix_checksum::generate(outbound_message));
@@ -30,10 +30,39 @@ fn checksum(message: &str) -> u32 {
 /// ```
 /// use fix_checksum::validate;
 ///
-/// assert_eq!(true, validate());
+/// // empty message
+/// assert_eq!(false, validate(""));
+///
+/// // no tail
+/// let inbound_message = "8=FIX.4.29=7335=049=BRKR56=INVMGR34=23552=19980604-07:58:28112=19980604-07:58:28";
+/// assert_eq!(false, validate(inbound_message));
+///
+/// // invalid checksum
+/// let inbound_message = "8=FIX.4.29=7335=049=BRKR56=INVMGR34=23552=19980604-07:58:28112=19980604-07:58:2810=231";
+/// assert_eq!(false, validate(inbound_message));
+///
+/// // invalid checksum format
+/// let inbound_message = "8=FIX.4.29=7335=049=BRKR56=INVMGR34=23552=19980604-07:58:28112=19980604-07:58:2810=2ZZ";
+/// assert_eq!(false, validate(inbound_message));
+///
+/// let inbound_message = "8=FIX.4.29=7335=049=BRKR56=INVMGR34=23552=19980604-07:58:28112=19980604-07:58:2810=236";
+/// assert_eq!(true, validate(inbound_message));
 /// ```
-pub fn validate() -> bool {
-  return true;
+pub fn validate(inbound_message: &str) -> bool {
+  if inbound_message.is_empty() { return false; }
+
+  let tail_start = inbound_message.find("\x01\x31\x30\x3D");
+  if tail_start.is_none() { return false; }
+
+  let split_index = tail_start.unwrap() + 1;
+  let (checksum_index_start, checksum_index_end) = (split_index + 3, split_index + 6);
+
+  let checksum_to_be = checksum(&inbound_message[..split_index]);
+  let checksum_as_is = inbound_message[checksum_index_start..checksum_index_end].parse::<u32>();
+
+  if checksum_as_is.is_err() { return false; }
+
+  return checksum_as_is.unwrap() == checksum_to_be;
 }
 
 /// This function generates checksum of FIX message
@@ -62,7 +91,23 @@ mod tests {
 
   #[test]
   fn it_should_validate_fix_message_checksum() {
-    assert_eq!(true, validate());
+    // empty message
+    assert_eq!(false, validate(""));
+
+    // no tail
+    let inbound_message = "8=FIX.4.29=7335=049=BRKR56=INVMGR34=23552=19980604-07:58:28112=19980604-07:58:28";
+    assert_eq!(false, validate(inbound_message));
+
+    // invalid checksum
+    let inbound_message = "8=FIX.4.29=7335=049=BRKR56=INVMGR34=23552=19980604-07:58:28112=19980604-07:58:2810=231";
+    assert_eq!(false, validate(inbound_message));
+
+    // invalid checksum format
+    let inbound_message = "8=FIX.4.29=7335=049=BRKR56=INVMGR34=23552=19980604-07:58:28112=19980604-07:58:2810=2ZZ";
+    assert_eq!(false, validate(inbound_message));
+
+    let inbound_message = "8=FIX.4.29=7335=049=BRKR56=INVMGR34=23552=19980604-07:58:28112=19980604-07:58:2810=236";
+    assert_eq!(true, validate(inbound_message));
   }
 
   #[test]
