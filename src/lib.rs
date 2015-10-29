6754d27236e8571ca9af988f26914a93d582172d
antklim <antklim@gmail.com>
2015-10-30 00:13:40 +1100

    Improvement: Added tests to general documentation

--- src/lib.rs
+++ src/lib.rs
@@ -8,7 +8,23 @@
 //! # Examples
 //!
 //! ```
+//! // empty message
 //! assert_eq!(false, fix_checksum::validate(""));
+//!
+//! // no tail
+//! let inbound_message = "8=FIX.4.29=7335=049=BRKR56=INVMGR34=23552=19980604-07:58:28112=19980604-07:58:28";
+//! assert_eq!(false, fix_checksum::validate(inbound_message));
+//!
+//! // invalid checksum
+//! let inbound_message = "8=FIX.4.29=7335=049=BRKR56=INVMGR34=23552=19980604-07:58:28112=19980604-07:58:2810=231";
+//! assert_eq!(false, fix_checksum::validate(inbound_message));
+//!
+//! // invalid checksum format
+//! let inbound_message = "8=FIX.4.29=7335=049=BRKR56=INVMGR34=23552=19980604-07:58:28112=19980604-07:58:2810=2ZZ";
+//! assert_eq!(false, fix_checksum::validate(inbound_message));
+//!
+//! let inbound_message = "8=FIX.4.29=7335=049=BRKR56=INVMGR34=23552=19980604-07:58:28112=19980604-07:58:2810=236";
+//! assert_eq!(true, fix_checksum::validate(inbound_message));
 //!
 //! let outbound_message = "8=FIX.4.2|9=73|35=0|49=BRKR|56=INVMGR|34=235|52=19980604-07:58:28|112=19980604-07:58:28|";
 //! assert_eq!("196", fix_checksum::generate(outbound_message));
